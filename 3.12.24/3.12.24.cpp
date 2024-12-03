11b00fc72b2469a9ef2bd87fa8acba7716d8558e
sarkis234 <sarkis95283@gmail.com>
2024-12-03 21:20:16 +0300

    Add files via upload

---
+++ 3.12.24/3.12.24.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+#include "List.h";
+
+int main()
+{
+	List<int> list;
+	list.pushBack(1);
+	list.pushStart(2);
+	list.pushStart(3);
+	list.pushStart(4);
+	list.pushStart(4);
+	list.pushStart(4);
+	list.pushStart(4);
+	list.print();
+	list.popBack();
+	list.print();
+	list.popStart();
+	list.print();
+	cout << list[1] << endl;
+	int* arr = list();
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i] << " ";
+	}
+}
