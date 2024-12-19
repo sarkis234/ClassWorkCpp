1192b8d1f242186c82e6cc8bee717bf9a3e2f3ab
sarkis234 <sarkis95283@gmail.com>
2024-12-19 21:13:56 +0300

    Add files via upload

---
+++ 19.12.24/19.12.24.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	string enWord, ruWord, line;
+	map<string, string> text;
+	string path1 = "dict.txt";
+	string path2 = "ru.txt";
+	string path3 = "en.txt";
+	ifstream in(path1);
+	int count = 0;
+	if (in.is_open())
+	{
+		while (getline(in, enWord, ':'))
+		{
+			getline(in, ruWord);
+			text.insert({ enWord, ruWord });
+			//cout << text[enWord];
+			
+		}
+	}
+	in.close();
+	ifstream in2(path3);
+	ofstream out(path2);
+	if (in2.is_open())
+	{
+		cout << "test";
+		while (in2 >> enWord)
+		{
+			if (enWord[enWord.length()] == ',') {
+
+			}
+			if (text.count(enWord)) {
+				ruWord = text[enWord];
+			}
+			else {
+				ruWord = enWord;
+			}
+			out << ruWord << " ";
+		}
+	}
+}
